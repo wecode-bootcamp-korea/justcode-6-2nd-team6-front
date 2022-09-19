31f7be3daea536c9cd5c60204767a6fbdfed7d20
choongmankim <cm730712@gmail.com>
2022-09-19 10:55:27 +0900

    [Add] 초기 세팅 추가

---
+++ florida/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Router from "./pages/Router";
+import "./styles/common.css";
+import "./styles/reset.css";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Router />);
---
+++ florida/src/pages/Router.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
