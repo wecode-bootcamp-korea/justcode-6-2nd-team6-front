fbed196ca01d5eddc120d4573800c6954e2b8f4f
choongmankim <cm730712@gmail.com>
2022-09-21 02:05:15 +0900

    [Add] arrow.svg components 추가

---
+++ src/pages/main/arrowIcon/NextArrow.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const NextArrow = () => {
+  return (
+    <svg
+      version="1.0"
+      xmlns="http://www.w3.org/2000/svg"
+      width="32.000000pt"
+      height="32.000000pt"
+      viewBox="0 0 32.000000 32.000000"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <g
+        transform="translate(0.000000,32.000000) scale(0.100000,-0.100000)"
+        fill="#000000"
+        stroke="none"
+      >
+        <path
+          d="M76 313 c-3 -4 26 -40 66 -80 l72 -73 -73 -74 c-49 -49 -69 -76 -61
+-81 14 -9 160 132 160 155 0 14 -137 160 -151 160 -3 0 -9 -3 -13 -7z"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default NextArrow;
