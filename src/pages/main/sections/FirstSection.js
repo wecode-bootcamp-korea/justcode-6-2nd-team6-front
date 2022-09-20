b0b4f5881d5fbda5fd1a2b70b6569cfaf842586f
choongmankim <cm730712@gmail.com>
2022-09-21 02:06:26 +0900

    [Add] 메인페이지 firstSection 슬라이더 기능 추가

--- src/pages/main/sections/FirstSection.js
+++ src/pages/main/sections/FirstSection.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import albumCover from "../../../Images/album-cover.jpg";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { BsFillPlayCircleFill } from "react-icons/bs";
 
 const StyledSection = styled.section`
@@ -14,16 +16,34 @@ const StyledSection = styled.section`
   }
 `;
 
-const StyledSlider = styled.div`
+const StyledSlider = styled(Slider)`
+  width: 70%
+  margin: 0 auto;
+
+  botton.slick-arrow.slick-prev {
+    width: 30px;
+    height: 30px;
+  }
+
+  .slick-prev:before {
+    color: black;
+    content: '<';
+  }
+  .slick-next:before {
+    color: black;
+    content: '>';
+  }
+
   div.first-section-slider-box {
     background-color: #4c4863;
     border-radius: 7px;
 
     a.first-section-slider-flex {
-      display: flex;
-      flex-direction: row;
       height: 350px;
       width: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-evenly;
       white-space: normal;
       background-position: 50%;
       background-repeat: no-repeat;
@@ -31,8 +51,8 @@ const StyledSlider = styled.div`
   }
 
   div.first-section-slider-info {
-    width: 360px;
-    padding: 50px 0 0 50px;
+    width: 280px;
+    padding: 40px 0 10px 0;
     font-weight: 800;
     line-height: 40px;
     color: #fff;
@@ -53,7 +73,7 @@ const StyledSlider = styled.div`
     }
 
     button.first-section-button {
-      margin-top: 50px;
+      margin-top: 70px;
       padding: 0;
       background: none;
       border: none;
@@ -68,12 +88,12 @@ const StyledSlider = styled.div`
   }
 
   div.first-section-playlist-wrap {
-    width: 100%
+    margin: auto 0;
   }
 
   ul.first-section-playlist-box {
     display: grid;
-
+    grid-template-columns: 250px 250px;
   }
 
   li.first-section-playlist-list {
@@ -86,114 +106,102 @@ const StyledSlider = styled.div`
       flex-direction: column;
       justify-content: center;
       line-height: 25px;
-      font-size: 18px;
+      font-size: 17px;
       color: white;
     }
   }
 
   img.first-section-album-cover {
-    width: 60px;
-    height: 60px;
+    width: 55px;
+    height: 55px;
+    margin: auto 0;
     border-radius: 7px;
   }  
 `;
 
 const FirstSection = () => {
+  const [titleList, setTitleList] = useState([]);
+  const [songList, setSongList] = useState([]);
+
+  useEffect(() => {
+    fetch("/data/slideData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setTitleList(data.slideData.slideTitleData);
+        setSongList(data.slideData.slideSongData);
+      });
+  }, []);
+
+  const settings = {
+    dots: true,
+    lazyLoad: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 2,
+  };
   return (
     <StyledSection>
       <section className="first-section-inner-box">
-        <StyledSlider>
-          <div className="first-section-wrap">
-            {/* 첫번째 슬라이드 */}
-            <div className="first-section-slider-box">
-              <a href="#" className="first-section-slider-flex">
-                {/* 플리 소개 */}
-                <div className="first-section-slider-info">
-                  <h4 className="first-section-slider-title">
-                    퇴근 후 쇠질엔 이만한 플리가 없지😎
-                  </h4>
-                  <div className="first-section-slider-date">
-                    총 14곡
-                    <span className="first-section-stick">|</span>
-                    2022.09.15
-                  </div>
-                  <button
-                    title="퇴근 후 쇠질엔 이만한 플리가 없지😎"
-                    type="button"
-                    className="first-section-button"
-                  >
-                    <BsFillPlayCircleFill className="first-section-play-button" />
-                  </button>
+        <StyledSlider {...settings}>
+          {titleList.map((result) => {
+            return (
+              <div key={result.titleId} className="first-section-wrap">
+                {/* 첫번째 슬라이드 */}
+                <div className="first-section-slider-box">
+                  <a href="#" className="first-section-slider-flex">
+                    {/* 플리 소개 */}
+                    <div className="first-section-slider-info">
+                      <h4 className="first-section-slider-title">
+                        {result.listTitle}
+                      </h4>
+                      <div className="first-section-slider-date">
+                        {result.listTotal}
+                        <span className="first-section-stick">|</span>
+                        {result.listDate}
+                      </div>
+                      <button
+                        title="퇴근 후 쇠질엔 이만한 플리가 없지😎"
+                        type="button"
+                        className="first-section-button"
+                      >
+                        <BsFillPlayCircleFill className="first-section-play-button" />
+                      </button>
+                    </div>
+                    {/* 노래리스트 */}
+                    <div className="first-section-playlist-wrap">
+                      <ul className="first-section-playlist-box">
+                        {songList.map((song) => {
+                          return (
+                            <li
+                              key={song.songId}
+                              className="first-section-playlist-list"
+                            >
+                              <img
+                                alt="앨범 표지"
+                                src={song.slideSongImg}
+                                className="first-section-album-cover"
+                              />
+                              <div className="first-section-playlist-box-info">
+                                <strong className="first-section-playlist-song">
+                                  {song.slideSongName}
+                                </strong>
+                                <div className="first-section-playlist-singer">
+                                  {song.slideSinger}
+                                </div>
+                              </div>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </div>
+                    {/* 플리 노래리스트 끝 */}
+                  </a>
                 </div>
-                {/* 노래리스트 */}
-                <div className="first-section-playlist-wrap">
-                  <ul className="first-section-playlist-box">
-                    <li className="first-section-playlist-list">
-                      <img
-                        alt="앨범 표지"
-                        src={albumCover}
-                        className="first-section-album-cover"
-                      />
-                      <div className="first-section-playlist-box-info">
-                        <strong className="first-section-playlist-song">
-                          Song
-                        </strong>
-                        <div className="first-section-playlist-singer">
-                          Eddie
-                        </div>
-                      </div>
-                    </li>
-                    <li className="first-section-playlist-list">
-                      <img
-                        alt="앨범 표지"
-                        src={albumCover}
-                        className="first-section-album-cover"
-                      />
-                      <div className="first-section-playlist-box-info">
-                        <strong className="first-section-playlist-song">
-                          Song
-                        </strong>
-                        <div className="first-section-playlist-singer">
-                          Eddie
-                        </div>
-                      </div>
-                    </li>
-                    <li className="first-section-playlist-list">
-                      <img
-                        alt="앨범 표지"
-                        src={albumCover}
-                        className="first-section-album-cover"
-                      />
-                      <div className="first-section-playlist-box-info">
-                        <strong className="first-section-playlist-song">
-                          Song
-                        </strong>
-                        <div className="first-section-playlist-singer">
-                          Eddie
-                        </div>
-                      </div>
-                    </li>
-                    <li className="first-section-playlist-list">
-                      <img
-                        alt="앨범 표지"
-                        src={albumCover}
-                        className="first-section-album-cover"
-                      />
-                      <div className="first-section-playlist-box-info">
-                        <strong className="first-section-playlist-song">
-                          Song
-                        </strong>
-                        <div className="first-section-playlist-singer">
-                          Eddie
-                        </div>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-                {/* 플리 노래리스트 끝 */}
-              </a>
-            </div>
-          </div>
+              </div>
+            );
+          })}
         </StyledSlider>
       </section>
     </StyledSection>
