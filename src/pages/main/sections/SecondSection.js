64a9c45af258ef336979fec0151e245892ac2dfb
choongmankim <cm730712@gmail.com>
2022-09-23 23:49:01 +0900

    [Fix] secondSection 버튼 및 hove 추가

--- src/pages/main/sections/SecondSection.js
+++ src/pages/main/sections/SecondSection.js
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import next from "../../../Images/next.png";
-import prev from "../../../Images/prev.png";
-import { BsFillPlayFill } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { BsFillPlayFill } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 const StyledSection = styled.section`
   width: 100%;
   margin: 50px auto;
   display: block;
+  font-family: 'NanumBarunGothic', sans-serif;
 
   section.second-section-inner-box {
     div.second-section-info-box {
@@ -23,18 +22,16 @@ const StyledSection = styled.section`
       flex-direction: row;
       align-items: center;
 
-
       button {
         border: none;
         background: none;
-        font-size: medium;
-        cursor: pointer;
-      }
+        font-size: large;
+        font-weight: 550;
 
-
-      img {
-        width: 20px;
-        height: 20px;
+        &:hover {
+          cursor: pointer;
+          color: #3f3fff;
+        }
       }
     }
 
@@ -44,98 +41,99 @@ const StyledSection = styled.section`
       margin: 30px 0;
 
       div.second-section-info-box {
-        
       }
     }
 
-      /* 발매 음악 앨범 타이틀 */
-      div.second-section-title-box {
-        width: 200px;
+    /* 발매 음악 앨범 타이틀 */
+    div.second-section-title-box {
+      width: 200px;
 
-        h3.second-section-title {
-          width: 100%;
-          display: inline-block;
-          font-size: 26px;
-          font-weight: normal;
-          color: #333;
+      h3.second-section-title {
+        width: 100%;
+        display: inline-block;
+        font-size: 26px;
+      }
+
+      a.second-section-title-link {
+        font-weight: 600;
+        color: black;
+
+        &:hover {
           cursor: pointer;
-          }
-        
-        a.second-section-title-link {
-
-        }
-        
-        span.second-section-today-music {
-
-        }
-
-        img.second-section-title-info {
-          width: 28px;
-          height: 14px;
-          margin-left: 5px;
+          color: #3f3fff;
         }
       }
 
-      div.second-section-album-wrap {
-        display: flex;
-        flex-diretion: row;
-        flex-wrap: wrap;
-        justify-content: space-between;
+      .second-section-today-music-icon {
+        width: 26px;
+        height: 24px;
+        margin-bottom: 2px;
+        margin-left: 8px;
+        text-indent: 100%;
+        vertical-align: middle;
+      }
+    }
+
+    /* 최신 발매 음악 앨범 리스트 */
+
+    div.second-section-album-wrap {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      justify-content: space-between;
+    }
+
+    div.second-section-album-box {
+      width: 192px;
+      background-clip: content-box;
+      margin-bottom: 20px;
+
+      div.second-section-album-list {
+        position: relative;
+        margin-bottom: 10px;
       }
 
-      div.second-section-album-box {
-        width: 192px;
-        background-clip: content-box;
-        margin-bottom: 20px;
+      /* 발매 음악 앨범 리스트 앨범 링크 */
+      a.second-section-album-link {
+        position: relative;
+        z-index: auto;
 
-        div.second-section-album-list {
+        div.second-section-album-img-box {
           position: relative;
-          margin-bottom: 10px;
-        }
-
-        /* 발매 음악 앨범 리스트 앨범 링크 */
-        a.second-section-album-link {
-          postion: relative;
-          z-index: auto;
-
-          div.second-section-album-img-box {
-            postion: relative;
-          }
-        }
-
-        img.second-section-album-cover {
-          width: 192px;
-          height: 192px;
-          border-radius: 7px;
-        }
-
-        /* 발매 음악 앨범 리스트 재생 버튼 */
-        button.second-section-play-button {
-          position: absolute;
-          z-index: auto;
-          right: -5px;
-          bottom: -1px;
-          border: none;
-          background: none;
-
-          .second-section-play-button-icon {
-            width: 55px;
-            height: 55px;
-            color: white;
-          }
         }
       }
-      a.second-section-album-song-link {
-        display: block;
-        margin-bottom: 8px;
-        margin-left: 5px;
+
+      img.second-section-album-cover {
+        width: 192px;
+        height: 192px;
+        border-radius: 7px;
       }
 
-      a.second-section-album-singer-link {
-        color: #999999;
-        font-size: large;
-        margin-left: 5px;
+      /* 발매 음악 앨범 리스트 재생 버튼 */
+      button.second-section-play-button {
+        position: absolute;
+        z-index: auto;
+        right: -5px;
+        bottom: -1px;
+        border: none;
+        background: none;
+
+        .second-section-play-button-icon {
+          width: 55px;
+          height: 55px;
+          color: white;
+        }
       }
+    }
+    a.second-section-album-song-link {
+      display: block;
+      margin-bottom: 8px;
+      margin-left: 5px;
+    }
+
+    a.second-section-album-singer-link {
+      color: #999999;
+      margin-left: 5px;
     }
   }
 `;
@@ -144,7 +142,7 @@ const SecondSection = () => {
   const [albumList, setAlbumList] = useState([]);
 
   useEffect(() => {
-    fetch("/data/releaseData.json")
+    fetch('/data/releaseData.json')
       .then((res) => res.json())
       .then((data) => {
         setAlbumList(data.releaseData);
@@ -153,104 +151,87 @@ const SecondSection = () => {
 
   return (
     <StyledSection>
-      <section className="second-section-inner-box">
+      <section className='second-section-inner-box'>
         {/*오늘 발매 음악 정보 */}
-        <div className="second-section-info-box">
+        <div className='second-section-info-box'>
           {/*오늘 발매 음악 제목 */}
-          <div className="second-section-title-box">
-            <h3 className="second-section-title">
-              <Link to="/detail" className="second-section-title-link">
-                <span className="second-section-today-music">
-                  오늘 발매 음악
+          <div className='second-section-title-box'>
+            <h3 className='second-section-title'>
+              <Link to='/detail' className='second-section-title-link'>
+                <span className='second-section-today-music'>
+                  최신 발매 음악
                 </span>
                 <img
-                  alt="오늘 발매 음악"
-                  className="second-section-title-info"
-                  src={next}
+                  alt='오늘 발매 음악'
+                  className='second-section-today-music-icon'
+                  src='/Images/next.png'
                 />
               </Link>
             </h3>
           </div>
           {/*오늘 발매 음악 장르 */}
-          <div className="second-section-genre">
-            <div className="second-section-genre-title">
+          <div className='second-section-genre'>
+            <div className='second-section-genre-title'>
               <button
-                alt="종합"
-                type="button"
-                id="genre-title"
-                className="second-section-genre-mix"
+                alt='종합'
+                type='button'
+                id='genre-title'
+                className='second-section-genre-mix'
               >
                 종합
               </button>
               <button
-                alt="국내"
-                type="button"
-                id="genre-title"
-                className="second-section-genre-domestic"
+                alt='국내'
+                type='button'
+                id='genre-title'
+                className='second-section-genre-domestic'
               >
                 국내
               </button>
               <button
-                alt="해외"
-                type="button"
-                id="genre-title"
-                className="second-section-genre-foreign"
+                alt='해외'
+                type='button'
+                id='genre-title'
+                className='second-section-genre-foreign'
               >
                 해외
               </button>
             </div>
-            {/*오늘 발매 음악 장르 슬라이더 */}
-            <div className="second-section-genre-arrow">
-              <button type="button" className="second-section-genre-prev">
-                <img
-                  alt="왼쪽"
-                  className="second-section-title-info"
-                  src={prev}
-                />
-              </button>
-              <button type="button" className="second-section-genre-next">
-                <img
-                  alt="오른쪽"
-                  className="second-section-title-info"
-                  src={next}
-                />
-              </button>
-            </div>
           </div>
         </div>
-        {/*오늘 발매 음악 정보 끝 */}
-        <div className="second-section-album-inner-box">
-          <div className="second-section-album-wrap">
+        {/*최신 발매 음악 정보 끝 */}
+        <div className='second-section-album-inner-box'>
+          <div className='second-section-album-wrap'>
             {/*앨범리스트*/}
             {albumList.map((result) => {
               return (
                 <div
                   key={result.releaseId}
-                  className="second-section-album-box"
+                  className='second-section-album-box'
                 >
-                  <div className="second-section-album-list">
-                    <Link to="/detail" className="second-section-album-link">
-                      <div className="second-section-album-img-box">
+                  <div className='second-section-album-list'>
+                    <Link to='/detail' className='second-section-album-link'>
+                      <div className='second-section-album-img-box'>
                         <img
-                          className="second-section-album-cover"
+                          className='second-section-album-cover'
                           src={result.releaseAlbum}
                         />
                       </div>
                     </Link>
                     <button
-                      alt="플레이 버튼"
-                      className="second-section-play-button"
+                      alt='플레이 버튼'
+                      className='second-section-play-button'
                     >
-                      <BsFillPlayFill className="second-section-play-button-icon" />
+                      <BsFillPlayFill className='second-section-play-button-icon' />
                     </button>
                   </div>
-                  <a className="second-section-album-song-link">
-                    <span className="second-section-song">
+                  <a className='second-section-album-song-link'>
+                    <span className='second-section-song'>
                       {result.releaseSong}
                     </span>
                   </a>
-                  <a className="second-section-album-singer-link">
-                    <span className="second-section-album-singer">
+                  <a className='second-section-album-singer-link'>
+                    <span className='second-section-album-singer'>
                       {result.releaseSinger}
                     </span>
                   </a>
