ccc755de70bf7ad3438804d57eb440fefa7b6b90
Roxy100 <misoamy16@gmail.com>
2022-09-23 15:58:51 +0900

    [Add]: 이용권 페이지 - MY 탭 UI 구현

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -1,30 +1,32 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import Login from "./login/Login";
-import Certification from "./signup/Certification";
-import Signform from "./signup/Signform";
-import Signup from "./signup/Signup";
-import Terms from "./signup/Terms";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Login from './login/Login';
+import Certification from './signup/Certification';
+import Signform from './signup/Signform';
+import Signup from './signup/Signup';
+import Terms from './signup/Terms';
 
-import Playbar from "../components/playbar/Playbar";
-import Purchase from "./purchase/Purchase";
-import Voucher from "./purchase/Voucher";
-import Affiliate from "./purchase/Affiliate";
+import Playbar from '../components/playbar/Playbar';
+import Purchase from './purchase/Purchase';
+import Voucher from './purchase/Voucher';
+import Affiliate from './purchase/Affiliate';
+import My from './purchase/My';
 
 function Router() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/certification" element={<Certification />} />
-        <Route path="/signform" element={<Signform />} />
-        <Route path="/purchase" element={<Purchase />}>
-          <Route path="voucher" element={<Voucher />}></Route>
-          <Route path="affiliate" element={<Affiliate />}></Route>
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/terms' element={<Terms />} />
+        <Route path='/certification' element={<Certification />} />
+        <Route path='/signform' element={<Signform />} />
+        <Route path='/purchase' element={<Purchase />}>
+          <Route path='voucher' element={<Voucher />}></Route>
+          <Route path='affiliate' element={<Affiliate />}></Route>
+          <Route path='my' element={<My />}></Route>
         </Route>
       </Routes>
       <Footer />
